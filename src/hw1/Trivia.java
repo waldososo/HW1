d93076eeafa20068ee027847f202670045496510
jkwoods <woodsjk@seas.upenn.edu>
2025-01-16 19:19:14 -0500

    fix

---
+++ src/hw1/Trivia.java
@@ -0,0 +1,51 @@
+package hw1;
+
+public class Trivia {
+
+	public static void main(String[] args) {
+
+		// TODO: Declare an int variable called `lateDays`;
+		// TODO: Set `lateDays` to the correct number based on the syllabus
+
+		// TODO: uncomment this line to see if lateDays prints correctly
+		// System.out.println("The number of free late days you get throughout the
+		// semester is " + lateDays);
+
+		// TODO: Declare a String variable called `woodsEmail`;
+		// TODO: Set `woodsEmail` to Mr. Woods' email address in the syllabus
+
+		// TODO: uncomment this line to see if woodsEmail prints correctly
+		// System.out.println("I can always reach my instructor at " + woodsEmail);
+
+		double celcius;
+		celcius = 15.0;
+
+		// TODO: Declare a double variable called `fahrenheit`.
+		// TODO: Write an expression that calculates the correct fahrenheit temperature
+		// from `celsius`
+		// Note you should NOT calculate the conversion on your own and put the number
+		// in. Make the code do the work.
+
+		// TODO: uncomment this line to see if temperature variables prints correctly
+		// System.out.println(celcius + " degrees celcius is " + fahrenheit + " degrees
+		// fahrenheit.");
+
+		// TODO: declare an int variable called `a` and an int variable called `b`
+		// TODO: set `a` to 100 and set `b` to a multiplied by 3, using an expression
+
+		// TODO: uncomment the following lines and edit the print statements so that
+		// if a is less than b, "a is smaller" prints out
+		// and if b is less than or equal to a, "b is smaller" prints out
+
+		/*
+		 * if (a < b) { System.out.println(""); } else { System.out.println(""); }
+		 */
+
+		// TODO: Write your own print statement(s) that complete the sentence
+		// "A project I would like to program is " ...
+		// This can be something you want to do in this class or something you know is
+		// a long way down the road but that you would want to make someday
+
+	}
+
+}
